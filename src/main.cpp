93f3c2fad49c7241d7cb5e284494d184233920f0
Rui Cruz <rpsc16@gmail.com>
2024-05-01 01:36:31 +0100

    Corrigir compilação

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "Interface.h"
+#include <iostream>
 
 int main() {
+    /*
     Interface interface;
     Interface::run();
+     */
+    std::cout << "Teste de compilação." << std::endl;
     return 0;
 }
